5c1ed4eae853ca90b1bef79caf89db092b33093f
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-10-16 02:33:38 +0200

    Change api to single object containing all puzzle functions

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,3 +1,7 @@
+import constraintTypes from "./constraintTypes";
+import * as cs from "csp-solver";
+import presets from "./presets";
+
 export default class Puzzle {
     constructor() {
         this.domains = [];
@@ -11,5 +15,55 @@ export default class Puzzle {
         this.colors = {};
         this.constraints = [];
         this.backgroundLayers = [];
+    }
+
+    getConstraints(puzzle) {
+        let constraints = [];
+        for (let c of puzzle.constraints) {
+            if (constraintTypes.hasOwnProperty(c.type)) {
+                constraints.push(...constraintTypes[c.type](puzzle.domain, c));
+            } else {
+                console.warn("Constraint", c, "was ignored, it's not supported");
+            }
+        }
+        return constraints;
+    }
+
+    getCSP(puzzle, solutions = 1) {
+        let constraints = this.getConstraints(puzzle);
+        return {
+            variables: puzzle.domains,
+            constraints,
+            mrv: true,
+            degree: false,
+            lcv: false,
+            solutions,
+        };
+    }
+
+    solve(puzzle) {
+        let csp = this.getCSP(puzzle, 1);
+        return cs.solve(csp);
+    }
+
+    hasUniqueSolution(puzzle) {
+        let csp = this.getCSP(puzzle, 'all');
+        let result = cs.solve(csp);
+        let unique = result.solutions.length === 1;
+        return {unique, result};
+    }
+
+    static presets() {
+        return presets;
+    }
+
+    static fromJSON(obj) {
+        if (typeof obj === 'string')
+            obj = JSON.parse(obj);
+        let puzzle = new Puzzle();
+        for (let key in obj)
+            if (obj.hasOwnProperty(key))
+                puzzle[key] = obj[key];
+        return puzzle;
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,43 +1,4 @@
-import * as cs from "csp-solver";
-import constraintTypes from "./constraintTypes";
 import PuzzleConstraint from "./PuzzleConstraint";
 import Puzzle from "./Puzzle";
 
-const getConstraints = (puzzle) => {
-    let constraints = [];
-    for (let c of puzzle.constraints) {
-        if (constraintTypes.hasOwnProperty(c.type)) {
-            constraints.push(...constraintTypes[c.type](puzzle.domain, c));
-        } else {
-            console.warn("Constraint", c, "was ignored, it's not supported");
-        }
-    }
-    return constraints;
-}
-
-const getCSP = (puzzle, solutions = 1) => {
-    let constraints = getConstraints(puzzle);
-    return {
-        variables: puzzle.domains,
-        constraints,
-        mrv: true,
-        degree: false,
-        lcv: false,
-        solutions,
-    };
-}
-
-const solve = (puzzle) => {
-    let csp = getCSP(puzzle, 1);
-    console.log(csp);
-    return cs.solve(csp);
-}
-
-const hasUniqueSolution = (puzzle) => {
-    let csp = getCSP(puzzle, 'all');
-    let result = cs.solve(csp);
-    let unique = result.solutions.length === 1;
-    return {unique, result};
-}
-
-export {getCSP, solve, hasUniqueSolution, getConstraints, PuzzleConstraint, Puzzle};
+export {PuzzleConstraint, Puzzle};
